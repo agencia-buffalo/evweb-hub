dd3be55b2a53f7b0ccd85a673724c262a1df535e
Rodrigo Zungri <biozungri@gmail.com>
2025-05-15 16:14:01 -0300

    updated

--- src/js/main.js
+++ src/js/main.js
@@ -67,3 +67,44 @@ document.addEventListener('DOMContentLoaded', () => {
     nextButton.addEventListener('click', showNextSlide);
     prevButton.addEventListener('click', showPrevSlide);
 });
+
+/* Counter */
+document.addEventListener('DOMContentLoaded', () => {
+    const counters = document.querySelectorAll('.counter');
+    const options = {
+        root: null,
+        threshold: 0.1
+    };
+
+    const animateCounter = (counter) => {
+        const target = +counter.getAttribute('data-target');
+        const duration = 2000; // Duración de la animación en milisegundos
+        const increment = target / (duration / 16); // Incremento por frame (~16ms por frame)
+
+        let current = 0;
+
+        const updateCounter = () => {
+            current += increment;
+            if (current < target) {
+                counter.textContent = Math.floor(current);
+                requestAnimationFrame(updateCounter);
+            } else {
+                counter.textContent = target; // Asegurarse de que el valor final sea exacto
+            }
+        };
+
+        updateCounter();
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const counter = entry.target;
+                animateCounter(counter);
+                observer.unobserve(counter); // Dejar de observar después de animar
+            }
+        });
+    }, options);
+
+    counters.forEach(counter => observer.observe(counter));
+});
